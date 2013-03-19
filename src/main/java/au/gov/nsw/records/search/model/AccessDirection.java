a932bd0646c305ddd52ea58a03051428ba307ac8
wisanup <airgear01@gmail.com>
2013-03-20 08:40:18 +1100

    Added JSON rendering for Access Direction

--- src/main/java/au/gov/nsw/records/search/model/AccessDirection.java
+++ src/main/java/au/gov/nsw/records/search/model/AccessDirection.java
@@ -11,22 +11,45 @@ import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
 import org.springframework.roo.addon.tostring.RooToString;
 
+import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.Expose;
+
 @RooJavaBean
 @RooToString
 @RooJpaActiveRecord(versionField = "")
 @Table(name = "accessdirections_view")
 public class AccessDirection {
 
+	@Expose
 	private String scope;
+	
+	@Expose
 	private String furtherDetails;
+	
+	@Expose
 	private String agencyTitle;
+	
+	@Expose
 	private String typeAccess;
+	
+	@Expose
 	private String effect;
+	
+	@Expose
 	private String cpaDuration;
+	
+	@Expose
 	private String cpaReasons;
+	
+	@Expose
 	private String location;
 	
+	@Expose
 	@Temporal(TemporalType.TIMESTAMP)
 	@DateTimeFormat(style = "M-")
 	private Date authorisationDate;
+	
+	public String getJsonString(){
+ 	 return new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create().toJson(this); 
+  }
 }
