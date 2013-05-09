666d11a484ab1bef69d2044f821037f2fd83be44
wisanup <airgear01@gmail.com>
2013-05-09 13:02:00 +1000

    Added more details to location

--- src/main/java/au/gov/nsw/records/search/service/LocationHelper.java
+++ src/main/java/au/gov/nsw/records/search/service/LocationHelper.java
@@ -29,4 +29,23 @@ public class LocationHelper {
 		// default location
 		return "Western Sydney Records Centre, Kingswood";
 	}
+	
+	public static String getAddress(String location){
+		String loc = getSimpleLocation(location);
+		if (loc.startsWith("Western")){
+			return "Western Sydney Records Centre \n143 O'Connell Street KINGSWOOD NSW";
+		}else if (loc.startsWith("University of New England")){
+			return "University of New England Heritage Centre \nC. B. Newling Campus \nCnr Dangar and Kentucky Sts ARMIDALE NSW";
+		}else if (loc.startsWith("Outback")){
+			return "Outback Archives Broken Hill City Library \nBlende Street BROKEN HILL NSW";
+		}else if (loc.startsWith("Newcastle")){
+			return "Newcastle Region Library \nLaman Street NEWCASTLE NSW";
+		}else if (loc.startsWith("Charles Sturt")){
+			return "Charles Sturt University Regional Archives \nBlakemore Building Hely Avenue WAGGA WAGGA NSW";
+		}else if (loc.startsWith("Library, University of Wollongong")){
+			return "The Library, University of Wollongong \nNorthfields Avenue WOLLONGONG NSW";
+		}
+		
+		return "";
+	}
 }
